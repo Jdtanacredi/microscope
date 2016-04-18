0a22ff2e4615f2d2e8b5902a87f28218579d4f2d
Justin Tanacredi <jtanacredi@gmail.com>
2016-04-18 18:14:15 -0400

    convert edit and delete to meteor methods

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -5,24 +5,34 @@ Template.postEdit.events({
     var currentPostId = this._id;
 
     var postProperties = {
+      _id: currentPostId,
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     }
 
-    Posts.update(currentPostId, {$set: postProperties}, function(error) {
-      if (error) {
-        alert(error.reason);
-      } else {
-        Router.go('postPage',{_id: currentPostId});
-      }
+    Meteor.call('postEdit', postProperties, function(error, result) {
+      if (error)
+        return alert(error.reason)
+
+      Router.go('postPage', {_id: result._id});
     });
   },
   'click .delete': function(e) {
     e.preventDefault();
     if (confirm("Delete this post?")) {
       var currentPostId = this._id;
-      Posts.remove(currentPostId);
-      Router.go('postsList');
+  //     Posts.remove(currentPostId);
+  //     Router.go('postsList');
+
+      Meteor.call('postDelete',currentPostId, function(error, result) {
+        if (error)
+          return alert(error.reason);
+
+        if (result.removed)
+          Router.go('postsList');
+
+      });
+
     }
   }
 });
